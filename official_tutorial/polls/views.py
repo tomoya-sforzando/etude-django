086854175ed45e0f70642ed317d47c9016dd7336
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-12-31 12:32:40 +0900

    feat(views.py): :+1: Create view of MVC
    Referenced [はじめての Django アプリ作成、その 3 | Django ドキュメント |
    Django](https://docs.djangoproject.com/ja/3.1/intro/tutorial03/)

--- official_tutorial/polls/views.py
+++ official_tutorial/polls/views.py
@@ -1,8 +1,38 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse  # , Http404
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # output = ", ".join([q.question_text for q in latest_question_list])
+    # template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    template = loader.get_template("polls/detail.html")
+    return HttpResponse(template.render({"question": question}, request))
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
